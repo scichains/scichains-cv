584e2c4a5bab76ce112b2ce3e8c9ef1e211e7f2b
Daniel Alievsky <daniel.alievsky@gmail.com>
2024-12-10 15:45:04 +0200

    Version 4.4.10

--- additional/usage_examples/execute_chain.cmd
+++ additional/usage_examples/execute_chain.cmd
@@ -2,6 +2,8 @@
 rem Should be called from result folder of the artifact
 set properties=-Dnet.algart.executors.path=net.algart.executors.core;net.algart.executors.cv
 set properties=%properties% -Dnet.algart.executors.api.showInfo=true
+set properties=%properties% -Dnet.algart.executors.check.existing.paths=false
+rem - We specify JAR files below in non-standard way and much disable built-in checking of class-paths
 set cp=./*
 echo on
 java -cp %cp% %properties% net.algart.executors.api.model.tests.callers.ExecutingChain %1 %2 %3 %4 %5
--- additional/usage_examples/execute_chain_example.cmd
+++ additional/usage_examples/execute_chain_example.cmd
@@ -1,9 +1,12 @@
 @echo off
 
+set SCICHAINS=/SciChains
+if not %1.==. set SCICHAINS=%1
+
 echo ----------
-echo NOTE: /chains/scichains-examples folder must exist and contain the following files:
-echo        /chains/scichains-examples/demo/image_processing/extract_face.chain
-echo        /chains/scichains-examples/demo/images/lenna.png
+echo NOTE: The actual version of SciChains application must be in installed in the folder %SCICHAINS%
+echo If it is installed in another folder, please specify it as the argument of this cmd-file:
+echo     execute_chain_example.cmd /actual_SciChains_folder
 echo %%TEMP%% environment variable should set to the OS temporary directory where the results will be saved in BMP files.
 echo Its current value is:
 echo %TEMP%
@@ -11,4 +14,4 @@ echo:
 pause
 echo:
 
-execute_chain /chains/scichains-examples/demo/image_processing/extract_face.chain /chains/scichains-examples/demo/images/lenna.png %TEMP% 1
+execute_chain %SCICHAINS%/monitor/demo/chains/image_processing/extract_face.chain %SCICHAINS%/monitor/demo/images/lenna.png %TEMP% 1
