c91801e29b125ef20c6bf40ed878787695e3ffa4
Daniel Alievsky <daniel.alievsky@gmail.com>
2025-08-26 12:59:32 +0300

    Artifact demo: removing warning about using JavaScript in interpretation mode

--- additional/usage_examples/execute_chain.cmd
+++ additional/usage_examples/execute_chain.cmd
@@ -4,6 +4,7 @@ set properties=-Dnet.algart.executors.path=net.algart.executors.core;net.algart.
 set properties=%properties% -Dnet.algart.executors.api.showInfo=true
 set properties=%properties% -Dnet.algart.executors.check.existing.paths=false
 rem - We specify JAR files below in non-standard way and much disable built-in checking of class-paths
+set properties=%properties% -Dpolyglot.engine.WarnInterpreterOnly=false
 set cp=./*
 echo on
 java --enable-native-access=ALL-UNNAMED -cp %cp% %properties% net.algart.executors.api.tests.ExecutingChain %1 %2 %3 %4 %5
